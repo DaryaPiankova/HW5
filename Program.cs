a448a2497b9fba953514887fb4898c3b5837b063
Daria <darya.piankova@yandex.ru>
2023-02-11 20:06:47 +0500

    task34

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+}
+
+
+int sum(int[] array)
+{
+    int count=0;
+  foreach (int element in array)
+  {
+    if (element%2==0)
+      count++;
+  }
+  return count;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(sum(array));
